22a8ffa1d8160b680c3d69465d02c94529fa332a
zsq259 <2530059525@qq.com>
2020-01-19 14:28:08 +0800

    Site updated: 2020-01-19 14:28:08

--- js/schemes/crash_cheat.js
+++ js/schemes/crash_cheat.js
@@ -1,17 +1,17 @@
 <!--崩溃欺骗-->
- var OriginTitle = document.title;
- var titleTime;
- document.addEventListener('visibilitychange', function () {
-     if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/img/TEP.ico");
-         document.title = '?你应该是一场梦';
-         clearTimeout(titleTime);
-     }
-     else {
-         $('[rel="icon"]').attr('href', "/favicon.ico");
-         document.title = '☀我应该是一阵风' + OriginTitle;
-         titleTime = setTimeout(function () {
-             document.title = OriginTitle;
-         }, 2000);
-     }
- });
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "/img/2.jpg");
+        document.title = '?';
+        clearTimeout(titleTime);
+    }
+    else {
+        $('[rel="icon"]').attr('href', "/img/2.jpg");
+        document.title = '☀' + OriginTitle;
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
